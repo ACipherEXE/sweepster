3fe8550cd5a0700697d3a4202add2bdd6fc8bdac
Christopher Negron <sonycar2000@outlook.com>
2024-01-13 13:19:43 -0500

    prod to dev

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import RoomPage from "./components/RoomPage/RoomPage";
 
 function App() {
   //Keep in dev unless wanting to test REST API
-  const enviroment = "prod";
+  const enviroment = "dev";
   const [currentArea, setCurrentArea] = useState("login");
   // eslint-disable-next-line
   const [isUserLogedIn, setIsUserLogedIn] = useState(true);
