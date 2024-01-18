be48d8f7515f1f2fcd424c23427ae43190e75943
Christopher Negron <sonycar2000@outlook.com>
2024-01-18 12:25:37 -0500

    Header and footer are set up

--- src/components/FooterArea/FooterArea.js
+++ src/components/FooterArea/FooterArea.js
@@ -8,18 +8,25 @@ function FooterArea(props) {
     <div class="footer-container">
       <div class="footer-item">
         <button
+          className="footer-home-button"
           onClick={() => {
-            setCurrentArea("floors");
+            // setCurrentArea("floors");
           }}
         >
-          ||||
+          <svg className="footer-home-icon" viewBox="0 0 576 512">
+            <path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path>
+          </svg>
         </button>
       </div>
       <div class="footer-item">
-        <div>Current Area</div>
+        <button className="new-multi-task-button">
+          <svg className="new-multi-task-button-icon" viewBox="0 0 448 512">
+            <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
+          </svg>
+        </button>
       </div>
       <div class="footer-item">
-        <button>profile</button>
+        <button>Unkown</button>
       </div>
     </div>
   );
