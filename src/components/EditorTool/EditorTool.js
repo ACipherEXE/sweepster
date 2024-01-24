a2bfb5a3f8277eefde14474e8a73503e8f273390
Christopher Negron <sonycar2000@outlook.com>
2024-01-24 18:57:23 -0500

    Implemented styles

--- src/components/EditorTool/EditorTool.js
+++ src/components/EditorTool/EditorTool.js
@@ -4,8 +4,13 @@ import "./EditorTool.css";
 function EditorTool() {
   return (
     <div class="editor-tool-container">
-      <div class="editor-tool-item">Grid Item 1</div>
-      <div class="editor-tool-item">Grid Item 2</div>
+      <div class="editor-tool-item">
+        <button className="editor-all-button">All</button>
+        <button className="editor-edit-button">Edit</button>
+      </div>
+      <div class="editor-tool-item">
+        <button className="editor-reset-button">Reset All Tasks</button>
+      </div>
     </div>
   );
 }
