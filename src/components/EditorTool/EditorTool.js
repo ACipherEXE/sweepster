c86a033df9dd6ba4d207e696e8eb6bb4d89d6e64
Christopher Negron <sonycar2000@outlook.com>
2024-01-24 18:49:07 -0500

    Made border on floors work and now working on editor component

---
+++ src/components/EditorTool/EditorTool.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./EditorTool.css";
+
+function EditorTool() {
+  return (
+    <div class="editor-tool-container">
+      <div class="editor-tool-item">Grid Item 1</div>
+      <div class="editor-tool-item">Grid Item 2</div>
+    </div>
+  );
+}
+export default EditorTool;
