ba64532bb4030da2976319e9b3911f319d6c237f
Christopher Negron <sonycar2000@outlook.com>
2024-01-25 13:03:59 -0500

    back up of work

--- src/components/EditorTool/EditorTool.js
+++ src/components/EditorTool/EditorTool.js
@@ -1,16 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EditorTool.css";
-
-function EditorTool() {
+import { PageType } from "../../Tools/Types";
+/**
+ * Component that handles the edit mode trigger on the app.
+ * @param {String} currentArea - current area the user is at
+ * @param {Boolean} editMode - if the user has enabled edit mode. It is set here!
+ * @param {useState} setEditMode - To change the boolean of edit mode
+ * @returns
+ */
+function EditorTool(props) {
+  const { currentArea, editMode, hotelNumber, setEditMode } = props;
+  const [isHotelNumberVisible, setIsHotelNumberVisible] = useState(false);
   return (
-    <div class="editor-tool-container">
-      <div class="editor-tool-item">
-        <button className="editor-all-button">All</button>
-        <button className="editor-edit-button">Edit</button>
-      </div>
-      <div class="editor-tool-item">
-        <button className="editor-reset-button">Reset All Tasks</button>
-      </div>
+    <div>
+      {/**
+       * Expand here the rooms you want this component to appear
+       */}
+      {currentArea === PageType.room && (
+        <div>
+          <div class="editor-tool-container">
+            <div class="editor-tool-item">
+              <button className="editor-all-button">All</button>
+              {editMode ? (
+                <button
+                  className="editor-edit-button-active"
+                  onClick={() => setEditMode(!editMode)}
+                >
+                  End Edit
+                </button>
+              ) : (
+                <button
+                  className="editor-edit-button"
+                  onClick={() => setEditMode(!editMode)}
+                >
+                  Edit
+                </button>
+              )}
+            </div>
+            {/**
+             * TODO: Make this change whe we are in the  user management page
+             */}
+            {editMode ? (
+              <div className="hotel-id-container">
+                <div
+                  className="editor-tool-item"
+                  onClick={() => setIsHotelNumberVisible(!isHotelNumberVisible)}
+                >
+                  <div className="editor-hotel-id">
+                    {isHotelNumberVisible ? hotelNumber : "Hotel ID"}
+                  </div>
+                  <div className="editor-hotel-id-icon"></div>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <div class="editor-tool-item">
+                  <button className="editor-reset-button">
+                    Reset All Tasks
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
