8cd568c65de3cca10d98dc96a1ecd130f58c2d93
Christopher Negron <sonycar2000@outlook.com>
2024-01-29 23:12:00 -0500

    Back up of work

--- src/components/AddTaskOverlay/AddTaskOverlay.js
+++ src/components/AddTaskOverlay/AddTaskOverlay.js
@@ -3,13 +3,21 @@ import "./AddTaskOverlay.css";
 import { PageType } from "../../Tools/Types";
 
 function AddTaskOverlay(props) {
-  var { currentArea, setCurrentArea } = props;
+  var { currentArea, setCurrentArea, setIsVisible } = props;
   return (
     <div className="add-task-overlay">
       <div className="add-task-box">
-        <div>hhello world</div>
-        <div>hhello world</div>
-        <div>hhello world</div>
+        <div className="add-task-question">
+          What task you would like to add?
+        </div>
+        <input
+          className="add-task-input"
+          type="text"
+          placeholder="Type here..."
+        />
+        <button className="add-task-button" onClick={() => setIsVisible(false)}>
+          Done
+        </button>
       </div>
     </div>
   );
