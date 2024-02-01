7e2f1d04d2f14611a6e102075006e036c36432a3
Christopher Negron <sonycar2000@outlook.com>
2024-02-01 14:14:03 -0500

    fixes
    fixes

--- src/components/UserEditorPage/UserCard.js
+++ src/components/UserEditorPage/UserCard.js
@@ -14,13 +14,15 @@ function UserCard() {
   const editMode = false;
   return (
     <div className="user-card-container" onClick={toggleDropdown}>
-      <div class="grid-item">
-        <div className="user-card-name">Cipher Negron</div>
-      </div>
-
-      <div>
+      <div className="user-card-grid" onClick={toggleDropdown}>
         <div class="grid-item">
-          <div className="user-card-permissions-non-edit-mode">Manager</div>
+          <div className="user-card-name">Cipher Negron</div>
+        </div>
+
+        <div>
+          <div class="grid-item">
+            <div className="user-card-permissions-non-edit-mode">Manager</div>
+          </div>
         </div>
       </div>
       {isOpen && (
@@ -31,40 +33,6 @@ function UserCard() {
           <button className="user-card-button"> ello world</button>
         </div>
       )}
-
-      {/* <div class="grid-item-dropdown">
-          <div className={`dropdown ${isOpen ? "active" : ""}`}>
-            <span
-              className="user-card-permissions-chosen"
-              onClick={toggleDropdown}
-            >
-              {selectedItem ? selectedItem : "User"}
-            </span>
-            <div className="dropdown-content">
-              <div className="dropdown-item" onClick={() => selectItem("User")}>
-                <div className="user-card-permissions">User</div>
-              </div>
-              <div
-                className="dropdown-item"
-                onClick={() => selectItem("Manager")}
-              >
-                <div className="user-card-permissions">Manager</div>
-              </div>
-              <div
-                className="dropdown-item"
-                onClick={() => selectItem("Admin")}
-              >
-                <div className="user-card-permissions">Admin</div>
-              </div>
-              <div
-                className="dropdown-item"
-                onClick={() => selectItem("Admin")}
-              >
-                <div className="user-card-permissions-delete">Delete</div>
-              </div>
-            </div>
-          </div>
-        </div> */}
     </div>
   );
 }
