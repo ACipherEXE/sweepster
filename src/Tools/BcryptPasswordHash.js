37cc1ff222fef7c694a2b08cdf4af3b2e0fab70b
Alex Hedges <alexhedges561@gmail.com>
2024-02-15 19:56:00 -0500

    Adjusted logic to create user accounts locally & use bcrypt

--- src/Tools/BcryptPasswordHash.js
+++ src/Tools/BcryptPasswordHash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 //Type in the password you want to hash below
-const password = "test";
+const password = "Test321";
 const salt = bcrypt.genSaltSync(10);
 const hash = bcrypt.hashSync(password, salt);
 console.log(hash);
--- src/components/UserLogIn.js
+++ src/components/UserLogIn.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import bcrypt from 'bcryptjs'; // Import bcrypt
 import "./UserLogIn.css";
 import { PageType } from "../Tools/Types";
 
 function UserLogIn(props) {
-  // eslint-disable-next-line
   var { setCurrentArea, setUserRequest, setIsUserLogedIn } = props;
+
+  // Add state for email and password
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Sample user accounts
+  const users = {
+    "user1@example.com": "$2a$10$lgmb1VMFn43V3WqojJgG9uO9tk9Urd.pF2AvTs4OS9zuZ9DBV5ysG", //Password is Test321
+    "user2@example.com": "$2a$10$Hd33/vB0XDilIIZrzGK80OtOmpMuJ0JlL6ed5oeo3M6htMB2WNJi6" //Password is Test123
+  };
+
+  const handleLogin = () => {
+    if (users[email]) { // Check if the email exists in the users object
+      if (bcrypt.compareSync(password, users[email])) { // Compare the entered password with the hashed password
+        setCurrentArea(PageType.floor);
+        setIsUserLogedIn(true);
+      } else {
+        alert("Invalid password");
+      }
+    } else {
+      alert("Email not found");
+    }
+  };
+  
 
   return (
     <div>
       <div className="login-image" />
       <div className="default-text">Sweepster</div>
+
+      {/* Add fields for email and password */}
+      <input
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Enter your password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
       <button
         className="login-button"
-        onClick={() => {
-          setCurrentArea(PageType.floor);
-          setIsUserLogedIn(true);
-        }}
+        onClick={handleLogin}
       >
         Sign in with Email
       </button>
