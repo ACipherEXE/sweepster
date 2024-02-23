1f8261da8c00c6626b5b19210269ed42caee660c
Alex Hedges <alexhedges561@gmail.com>
2024-02-22 20:19:08 -0500

    Created logic for room container to respect multiple rows of rooms

--- src/components/FloorsPage/FloorDropdown.js
+++ src/components/FloorsPage/FloorDropdown.js
@@ -50,27 +50,28 @@ function FloorDropdown(props) {
             <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
           </svg>
         </div>
-        {isFloorCardOpen && (
-          <div className="floor-card-open">
-            {floorData.rooms.map((room) => {
-              return (
-                <div
-                  className={
-                    room.tasks.some((task) => task.isDone === false)
-                      ? "room-button-warning"
-                      : "room-button"
-                  }
-                  onClick={() => {
-                    setCurrentArea(PageType.room);
-                    setUserRequest({ floor: floorData.floor, room: room.room });
-                  }}
-                >
-                  {room.room}
-                </div>
-              );
-            })}
-          </div>
-        )}
+{isFloorCardOpen && (
+  <div className="floor-card-open room-grid">
+    {floorData.rooms.map((room) => {
+      return (
+        <div
+          className={
+            room.tasks.some((task) => task.isDone === false)
+              ? "room-button-warning"
+              : "room-button"
+          }
+          onClick={() => {
+            setCurrentArea(PageType.room);
+            setUserRequest({ floor: floorData.floor, room: room.room });
+          }}
+        >
+          {room.room}
+        </div>
+      );
+    })}
+  </div>
+)}
+
       </div>
     </>
   );
