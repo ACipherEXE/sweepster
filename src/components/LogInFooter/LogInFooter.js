446b793c9e6726764705b862de777ea4b53701d3
Alex Hedges <alexhedges561@gmail.com>
2024-03-12 00:07:02 -0400

    Back and Next buttons hidden on log-in step

--- src/components/LogInFooter/LogInFooter.js
+++ src/components/LogInFooter/LogInFooter.js
@@ -161,7 +161,7 @@ function LogInFooter(props) {
   function goAStepBack() {
     console.log(loginStep);
     if (loginStep === "sign-up-username") {
-      setLoginStep("login");
+      setLoginStep("log-in");
     }
     if (loginStep === "sign-up-password") {
       setLoginStep("sign-up-username");
@@ -185,24 +185,26 @@ function LogInFooter(props) {
   }
   return (
     <div className="log-in-footer-container">
+      {loginStep !== "log-in" && (
+        <button
+          className="login-back-step-button"
+          onClick={() => {
+            goAStepBack();
+          }}
+        >
+          Back
+        </button>
+      )}
       <button
-        className="login-back-step-button"
-        onClick={() => {
-          goAStepBack();
-        }}
+        className="new-multi-task-button-login"
+        onClick={() => setIsVisible(true)}
+        style={{ display: loginStep === "tasks-for-all-rooms" ? "block" : "none" }}
       >
-        Back
+        <svg className="new-multi-task-button-icon" viewBox="0 0 448 512">
+          <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
+        </svg>
       </button>
-      <button
-  className="new-multi-task-button-login"
-  onClick={() => setIsVisible(true)}
-  style={{ display: loginStep === "tasks-for-all-rooms" ? "block" : "none" }}
->
-  <svg className="new-multi-task-button-icon" viewBox="0 0 448 512">
-    <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
-  </svg>
-</button>
-
+  
       {isVisible && (
         <div>
           <AddTaskOverlay
@@ -213,15 +215,18 @@ function LogInFooter(props) {
           />
         </div>
       )}
-      <button
-        className="login-next-step-button"
-        onClick={() => {
-          goAStepFront();
-        }}
-      >
-        Next
-      </button>
+      {loginStep !== "log-in" && (
+        <button
+          className="login-next-step-button"
+          onClick={() => {
+            goAStepFront();
+          }}
+        >
+          Next
+        </button>
+      )}
     </div>
   );
+  
 }
 export default LogInFooter;
